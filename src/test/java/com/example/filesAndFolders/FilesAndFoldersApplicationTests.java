071619c70259b7b1c740d18e2615a8db75b1be54
Thomas Gracie <tgracie030@gmail.com>
2018-12-05 08:47:00 +0000

    Completed

--- src/test/java/com/example/filesAndFolders/FilesAndFoldersApplicationTests.java
+++ src/test/java/com/example/filesAndFolders/FilesAndFoldersApplicationTests.java
@@ -1,7 +1,15 @@
 package com.example.filesAndFolders;
 
+import com.example.filesAndFolders.models.File;
+import com.example.filesAndFolders.models.Folder;
+import com.example.filesAndFolders.models.User;
+import com.example.filesAndFolders.repositories.FileRepo;
+import com.example.filesAndFolders.repositories.FolderRepo;
+import com.example.filesAndFolders.repositories.UserRepo;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +17,43 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class FilesAndFoldersApplicationTests {
 
+	@Autowired
+	UserRepo userRepo;
+
+	@Autowired
+	FolderRepo folderRepo;
+
+	@Autowired
+	FileRepo fileRepo;
+
 	@Test
 	public void contextLoads() {
 	}
+
+	@Test
+    public void canCreateUser(){
+        User user = new User("Bob");
+        userRepo.save(user);
+    }
+
+    @Test
+    public void canCreateFolder(){
+	    User user = new User("Frankie");
+	    userRepo.save(user);
+	    Folder folder = new Folder("Notes", user);
+	    folderRepo.save(folder);
+    }
+
+    @Test
+    public void canCreateFile(){
+        User user = new User("Flarb");
+        userRepo.save(user);
+
+        Folder folder = new Folder("Nurts", user);
+        folderRepo.save(folder);
+
+	    File file = new File("Notes from today", ".word", 35, folder);
+	    fileRepo.save(file);
+    }
 
 }
